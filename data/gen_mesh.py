010ab52fd24e9b3f0e3e23d316fc35b9b59fbf09
BobW <bobw@norfolk.sidefx.com>
2019-02-22 16:00:55 -0500

    Added rgb support in meshlab

--- data/gen_mesh.py
+++ data/gen_mesh.py
@@ -8,29 +8,37 @@ from PIL import Image
 #     for i in range(len(x.shape)):
 #         i_memo[(x[i], y[i])] = i + 1
 
-def get_coords_3d(img, focal_len):
+def get_coords_3d_and_rgb(img, focal_len, rgb):
     has_depth = img != 0
     indices = np.asarray(has_depth.nonzero())
     x, y = indices[1], indices[0]
     z = img[y, x]
+    rgb = rgb[y, x, :]
     x, y, z = x.astype(np.float), y.astype(np.float), z.astype(np.float)
 
     center_x, center_y = (img.shape[1] - 1.0) / 2.0, (img.shape[0] - 1.0) / 2.0
     x = ((x - center_x) * z) / focal_len
     y = ((center_y - y) * z) / focal_len # y coords are flipped
 
-    return x, y, z
+    return np.column_stack((x,y,z)), rgb
 
-def generate_xyz(x, y, z, file_name):
-    if not file_name.endswith('.xyz'):
-        file_name += '.xyz'
-    with open(file_name, 'w+') as xyz:
-        for i in range(x.shape[0]):
-            xyz.write("{0} {1} {2}\n".format(x[i], y[i], z[i]))
+def generate_ply(xyz, rgb, file_name):
+    if not file_name.endswith('.ply'):
+        file_name += '.ply'
+
+    header = ["ply", "format ascii 1.0", "element vertex {0}".format(xyz.shape[0]), "property float x", "property float y", 
+    "property float z", "property uchar red", "property uchar green", "property uchar blue", "end_header"]
+
+    with open(file_name, 'w+') as ply:
+        for item in header:
+            ply.write("{0}\n".format(item))
+
+        for i in range(xyz.shape[0]):
+            ply.write("{0} {1} {2} {3} {4} {5}\n".format(xyz[i,0], xyz[i,1], xyz[i,2], rgb[i,0], rgb[i,1], rgb[i,2]))
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('--src', type=str, default='depth_16bit_comp/')
+    parser.add_argument('--src', type=str, default='test/')
     parser.add_argument('--dest', type=str, default='meshlab/')
     parser.add_argument('--focal', type=float, default=721.5377)
     args = parser.parse_args()
@@ -39,5 +47,6 @@ if __name__ == "__main__":
         os.makedirs(args.dest)
 
     img = np.array(Image.open(os.path.join(args.src, '82.png')))
-    x, y, z = get_coords_3d(img, args.focal)
-    generate_xyz(x,y,z, os.path.join(args.dest, '82.xyz'))
+    rgb = np.array(Image.open(os.path.join(args.src, '82_rgb.png')).convert('RGB').resize((1244, 376)))
+    xyz, rgb = get_coords_3d_and_rgb(img, args.focal, rgb)
+    generate_ply(xyz, rgb, os.path.join(args.dest, '82.ply'))
